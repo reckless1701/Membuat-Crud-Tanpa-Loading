3e2dd9a7fe838b34bb2836101fafd471308066df
Burhanudin Dwi Saputra <63339790+reckless1701@users.noreply.github.com>
2020-06-20 06:30:12 +0700

    hapus_data.php

---
+++ hapus_data.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+$id = $_POST['id'];
+
+$query = "DELETE FROM tb_siswa WHERE id=?";
+$prepare1 = $db1->prepare($query);
+$prepare1->bind_param("i", $id);
+$prepare1->execute();
+
+echo json_encode(['success'=>'Sukses']);
+
+$db1->close();
+?>
